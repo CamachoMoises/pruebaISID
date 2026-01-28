de59c0aef2e18d76d984a55c9df5056a5c8c3ffe
Moises Camacho <moisescamacho26@gmail.com>
2026-01-28 12:39:06 +0100

    Revert "deleting starter kit"
    This reverts commit 7ef5dcaecff1eada58707b689cf150d0eac3ac9a.

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,12 +1,14 @@
-import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '../css/app.css';
+import { initializeTheme } from './hooks/use-appearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => (title ? `${title} - ${appName}` : appName),
     resolve: (name) =>
         resolvePageComponent(
             `./pages/${name}.tsx`,
@@ -14,9 +16,17 @@ createInertiaApp({
         ),
     setup({ el, App, props }) {
         const root = createRoot(el);
-        root.render(<App {...props} />);
+
+        root.render(
+            <StrictMode>
+                <App {...props} />
+            </StrictMode>,
+        );
     },
     progress: {
         color: '#4B5563',
     },
 });
+
+// This will set light / dark mode on load...
+initializeTheme();
