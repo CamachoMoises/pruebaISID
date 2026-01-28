c8dcb40286c5a468ff4f7fac4c0032b2a3faa988
Moises Camacho <moisescamacho26@gmail.com>
2026-01-28 12:53:54 +0100

    deleting starter kit fix

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
+// Ruta vacía por ahora
 Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
-
-Route::get('dashboard', function () {
-    return Inertia::render('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__.'/settings.php';
+    return Inertia::render('Welcome');
+});
